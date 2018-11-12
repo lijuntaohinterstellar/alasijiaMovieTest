6e45fa051d4036581c901b3f5a6b5a9de8504668
lijuntaohinterstellar <lijuntao@hinterstellar.com>
2018-11-12 16:46:25 +0800

    上传测试文件

--- app/src/main/java/com/example/newsdog/alasijiatest/MainActivity.java
+++ app/src/main/java/com/example/newsdog/alasijiatest/MainActivity.java
@@ -1,13 +1,121 @@
 package com.example.newsdog.alasijiatest;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.SurfaceTexture;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.hw.photomovie.PhotoMovieAdapter;
+import com.hw.photomovie.PhotoMovieFactory;
+import com.hw.photomovie.PhotoMovieRenderer;
+import com.hw.photomovie.filter.FilterType;
+import com.hw.photomovie.render.GLTextureView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivityTest";
+    private GLTextureView mGLTextureView;
+    PhotoMovieAdapter mPhotoMovieAdapter = null;
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
+    private ArrayList<String> mImages = new ArrayList<String>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //设置全屏无状态栏，并竖屏显示
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mGLTextureView = new GLTextureView(this);
+        mPhotoMovieAdapter = new PhotoMovieRenderer(this, mGLTextureView);
+
+        mImages.add("/storage/emulated/0/DCIM/Compose20181026205024.jpg");
+        mImages.add("/storage/emulated/0/DCIM/Compose20181026151149.jpg");
+        mImages.add("/storage/emulated/0/DCIM/Compose20181106113729.jpg");
+
+        setContentView(mGLTextureView);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 添加图片序列
+                mPhotoMovieAdapter.addImages(mImages);
+
+            }
+        }, 500);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 切换动画效果
+                mPhotoMovieAdapter.setTransfermation(PhotoMovieFactory.PhotoMovieType.SCALE);
+
+            }
+        }, 3000);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 设置滤镜
+                mPhotoMovieAdapter.setFilter(FilterType.GRAY);
+
+            }
+        }, 5000);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 合成视频
+                mPhotoMovieAdapter.composeVideo("", "");
+
+            }
+        }, 7000);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 设置滤镜
+                mPhotoMovieAdapter.onPause();
+                mGLTextureView.onPause();
+                Log.i("mainActivity", "onPause");
+            }
+        }, 20000);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //
+                mPhotoMovieAdapter.onResume();
+                mGLTextureView.onResume();
+                Log.i("mainActivity", "onResume");
+
+            }
+        }, 23000);
+
+//        mHandler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                // 设置滤镜
+//                mDemoPresenter.addMusic("");
+//
+//            }
+//        }, 7000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
